1dffdd0f8c4abdf9cff6d282e3445cd5919879da
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-10-06 19:28:36 +0200

    dropdow lists display and close

---
+++ jsmodules/dropdownlists.js
@@ -0,0 +1,78 @@
+export function displayDropdownLists(recipes) {
+
+    ingredientsList(recipes)
+    appliancesList(recipes)
+    ustensilsList(recipes)
+
+    // close lists when click outside
+    function closeDetailsLists(event) {
+        document.querySelectorAll(".research-list").forEach(element => {
+            if (!element.contains(event.target)) {
+                element.removeAttribute("open");
+            }
+        });
+    };
+
+    document.addEventListener("click", (event) => {
+        closeDetailsLists(event)
+    });
+
+    function displayDropdownList(list, elementID) {
+
+        let dropdownListDOM = "";
+
+        list.map(element => {
+            dropdownListDOM += `<li>${element}</li> `
+        });
+
+        document.getElementById(elementID).innerHTML = dropdownListDOM;
+    }
+
+    function ingredientsList(recipes) {
+
+        let result = [];
+        for (let index = 0; index < recipes.length; index++) {
+            recipes[index].ingredients.map(ingredient => {
+                result.push(ingredient.ingredient)
+            })
+        }
+
+        let ingredientsList = [];
+        removeDuplicates(result, ingredientsList)
+        displayDropdownList(ingredientsList, "list-ingredients")
+    }
+
+    function appliancesList(recipes) {
+
+        let result = [];
+        for (let index = 0; index < recipes.length; index++) {
+            result.push(recipes[index].appliance)
+        }
+
+        let appliancesList = [];
+        removeDuplicates(result, appliancesList)
+        displayDropdownList(appliancesList, "list-appliances")
+    }
+
+    function ustensilsList(recipes) {
+
+        let result = [];
+        for (let index = 0; index < recipes.length; index++) {
+            recipes[index].ustensils.map(ustensil => {
+                result.push(ustensil)
+            })
+        }
+
+        let ustensilsList = [];
+        removeDuplicates(result, ustensilsList)
+        displayDropdownList(ustensilsList, "list-ustensils")
+    }
+
+    function removeDuplicates(list, newList) {
+        list.forEach(element => {
+            if (!newList.includes(element)) {
+                newList.push(element)
+            }
+        });
+    }
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+//modules
+import {recipes} from "./jsmodules/recipes.js";
+import {displayRecipes} from "./jsmodules/displayrecipes.js";
+import {displayDropdownLists} from "./jsmodules/dropdownlists.js";
+
+displayRecipes(recipes);
+displayDropdownLists(recipes);
