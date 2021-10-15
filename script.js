0a85413688341ab1f8c9daf75eb0b90a96793e1e
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-10-15 19:58:49 +0200

    include filter to dropdown list

--- script.js
+++ script.js
@@ -4,99 +4,117 @@ import {displayRecipes} from "./jsmodules/displayrecipes.js";
 import {displayDropdownLists} from "./jsmodules/dropdownlists.js";
 import {displayTags} from "./jsmodules/displaytags.js";
 
+
+let recipesList = recipes;
+
 //DOM display
-displayDropdownLists(recipes);
-displayRecipes(recipes);
+displayDropdownLists(recipesList);
+displayRecipes(recipesList);
 displayTags();
 
-//research-bar input
-const globalResearchBar = document.getElementById("main-research-input");
 
-globalResearchBar.addEventListener("input", () => {
-    if (globalResearchBar.value.length >= 3) {
-        globalResearch(globalResearchBar.value)
-    } else {
-        displayRecipes(recipes);
+
+//event listeners
+const inputResearchBar = document.getElementById("main-research-input");
+
+inputResearchBar.addEventListener("input", () => {
+    if (inputResearchBar.value.length >= 3) {
+        globalResearch(recipes)
     }
 });
 
-//get selct tags list
 document.addEventListener("click", (event) => {
-
-    let tagValuesList = [];
-
     if (event.target.className === "list-element primary-color" ||
     event.target.className === "list-element secondary-color" ||
     event.target.className === "list-element tertiary-color" ||
     event.target.className === "delete-tag-btn") {
-        document.querySelectorAll(".tag").forEach(tag => {
-            tagValuesList.push(tag.innerText);
-            
-        });
-        // let newTagValuesList = [...new Set(tagValuesList)]
-        
-        return tagResearch(tagValuesList);
+        globalResearch(recipes)
     }
 });
 
-// research functions
-function tagResearch(tags) {
-    if (tags.length === 0 && globalResearchBar.value.length < 3) {
-        displayRecipes(recipes);
-    } else {
-        let tagResearchResult = [];
 
-    tags.map(tag => {
-        search(tag.slice(0, -1).toLowerCase(), tagResearchResult);
-    })
+function globalResearch(recipes) {
 
-    // console.log(tagResearchResult)
-    //display result
-    return displayRecipes(tagResearchResult);
+    //grab the elements needeed for research : input value and tags list
+    let inputResearchBarContent = "";
+    let tagValuesList = [];
+    
+    //get input value
+    inputResearchBarContent = inputResearchBar.value
+    //get selected tag list
+    document.querySelectorAll(".tag").forEach(tag => {
+        tagValuesList.push(tag.innerText)  
+    });
+
+    //launch filters
+    tagFilter(tagValuesList)
+    inputFilter(inputResearchBarContent)
+        
+    console.log(inputResearchBarContent, tagValuesList)
+
+    function inputFilter(inputResearchBarContent) {
+
+        if (inputResearchBarContent !== "") {
+
+            let inputResearchResult = [];
+    
+            search(inputResearchBarContent, inputResearchResult);
+
+            //remove duplicates //https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/
+            let newinputResearchResult = [...new Set(inputResearchResult)]
+
+            displayRecipes(newinputResearchResult);
+            displayDropdownLists(newinputResearchResult);
+        }
+    }
+
+    function tagFilter(tagValuesList) {
+
+        if (tagValuesList.length !== 0) {
+
+            let tagResearchResult = [];
+    
+            tagValuesList.map(tag => {
+                search(tag.slice(0, -1).toLowerCase(), tagResearchResult);
+            })
+
+            displayRecipes(tagResearchResult)
+            displayDropdownLists(newinputResearchResult);
+        }
     }
 
     
-}
-
-function globalResearch(value) {
-
-    let globalResearchResult = [];
-
-    search(value, globalResearchResult);
-
-    //remove duplicates //https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/
-    let newGlobalResearchResult = [...new Set(globalResearchResult)]
-    // console.log(newGlobalResearchResult)
-    //display result
-    return displayRecipes(newGlobalResearchResult);
-}
-
-function search(value, list) {
-
-    recipes.map(recipe => {
-        
-        // research into recipes names
-        if (recipe.name.toLowerCase().includes(value)) {
-            list.push(recipe)
-        };
-
-        // research into recipes appliances
-        if (recipe.appliance.toLowerCase().includes(value)) {
-            list.push(recipe)
-        };
-
-        // research into recipes ingredients
-        recipe.ingredients.map(ingredient => {
-            if (ingredient.ingredient.toLowerCase().includes(value)) {
+    
+    function search(value, list) {
+    
+        recipes.map(recipe => {
+            
+            // research into recipes names
+            if (recipe.name.toLowerCase().includes(value)) {
                 list.push(recipe)
-            } 
-        });
-
-        // research into recipes ustensils
-        recipe.ustensils.map(ustensil => {
-            if (ustensil.toLowerCase().includes(value)) {
+            };
+    
+            // research into recipes appliances
+            if (recipe.appliance.toLowerCase().includes(value)) {
                 list.push(recipe)
-            } 
-        });
-    })
+            };
+    
+            // research into recipes ingredients
+            recipe.ingredients.map(ingredient => {
+                if (ingredient.ingredient.toLowerCase().includes(value)) {
+                    list.push(recipe)
+                } 
+            });
+    
+            // research into recipes ustensils
+            recipe.ustensils.map(ustensil => {
+                if (ustensil.toLowerCase().includes(value)) {
+                    list.push(recipe)
+                } 
+            });
+        })
+    }
+
+    
+
 }
