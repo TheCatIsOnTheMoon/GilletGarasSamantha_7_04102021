2b31acfedbf853d4a8664d9c49660a597f36bfac
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-10-20 17:57:15 +0200

    new version of search with filter

--- script.js
+++ script.js
@@ -1,120 +1,128 @@
 //modules
-import {recipes} from "./jsmodules/recipes.js";
-import {displayRecipes} from "./jsmodules/displayrecipes.js";
-import {displayDropdownLists} from "./jsmodules/dropdownlists.js";
-import {displayTags} from "./jsmodules/displaytags.js";
+import {
+    recipes
+} from "./jsmodules/recipes.js";
+import {
+    displayRecipes
+} from "./jsmodules/displayrecipes.js";
+import {
+    displayDropdownLists
+} from "./jsmodules/dropdownlists.js";
+import {
+    displayTags
+} from "./jsmodules/displaytags.js";
 
-
-let recipesList = recipes;
 
 //DOM display
-displayDropdownLists(recipesList);
-displayRecipes(recipesList);
+displayDropdownLists(recipes);
+displayRecipes(recipes);
 displayTags();
-
-
 
 //event listeners
 const inputResearchBar = document.getElementById("main-research-input");
 
 inputResearchBar.addEventListener("input", () => {
-    if (inputResearchBar.value.length >= 3) {
-        globalResearch(recipes)
-    }
+    research(recipes)
 });
 
 document.addEventListener("click", (event) => {
     if (event.target.className === "list-element primary-color" ||
-    event.target.className === "list-element secondary-color" ||
-    event.target.className === "list-element tertiary-color" ||
-    event.target.className === "delete-tag-btn") {
-        globalResearch(recipes)
+        event.target.className === "list-element secondary-color" ||
+        event.target.className === "list-element tertiary-color" ||
+        event.target.className === "delete-tag-btn") {
+        research(recipes)
     }
 });
 
+//initializing
+let filteredRecipes = recipes;
 
-function globalResearch(recipes) {
+function research(recipes) {
 
     //grab the elements needeed for research : input value and tags list
-    let inputResearchBarContent = "";
-    let tagValuesList = [];
-    
-    //get input value
-    inputResearchBarContent = inputResearchBar.value
-    //get selected tag list
+
+    let inputContent = inputResearchBar.value.toLowerCase() //get input value
+
+    let selectedTags = [];
     document.querySelectorAll(".tag").forEach(tag => {
-        tagValuesList.push(tag.innerText)  
+        selectedTags.push(tag.innerText) //get selected tag list
     });
 
-    //launch filters
-    tagFilter(tagValuesList)
-    inputFilter(inputResearchBarContent)
-        
-    console.log(inputResearchBarContent, tagValuesList)
+    // console.log(inputContent, selectedTags) // OK, exemple in console: string [ "array " ]
 
-    function inputFilter(inputResearchBarContent) {
+    if (inputContent.length < 3 && selectedTags.length === 0) {
 
-        if (inputResearchBarContent !== "") {
+        filteredRecipes = recipes;
 
-            let inputResearchResult = [];
-    
-            search(inputResearchBarContent, inputResearchResult);
+    } else {
 
-            //remove duplicates //https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/
-            let newinputResearchResult = [...new Set(inputResearchResult)]
+        filteredRecipes = filteredRecipes.filter((recipe) => {
 
-            displayRecipes(newinputResearchResult);
-            displayDropdownLists(newinputResearchResult);
-        }
-    }
+            // selected tags filter
+            let filterForEachTag = selectedTags.map(selectedtag => {
 
-    function tagFilter(tagValuesList) {
+                let tag = selectedtag.slice(0, -1).toLowerCase();
 
-        if (tagValuesList.length !== 0) {
+                // tag into recipe appliance ?
+                if (recipe.appliance.toLowerCase().includes(tag)) {
+                    return true;
+                };
 
-            let tagResearchResult = [];
-    
-            tagValuesList.map(tag => {
-                search(tag.slice(0, -1).toLowerCase(), tagResearchResult);
+                // tag content into recipe ingredients ?
+                let filteredRecipesIngredients = recipe.ingredients.filter(ingredient => {
+                    if (ingredient.ingredient.toLowerCase().includes(tag)) {
+                        return true;
+                    }
+                });
+
+                // tag content into recipe ustensils ?
+                let filteredRecipesUstensils = recipe.ustensils.filter(ustensil => {
+                    if (ustensil.toLowerCase().includes(tag)) {
+                        return true;
+                    }
+                });
+
+                //https://stackoverflow.com/questions/49698136/es5-filter-inside-filter
+                if (filteredRecipesIngredients.length > 0 || filteredRecipesUstensils.length > 0) {
+                    return true
+                } else {
+                    return false;
+                };
+
             })
 
-            displayRecipes(tagResearchResult)
-            displayDropdownLists(newinputResearchResult);
-        }
-    }
-
-    
-    
-    function search(value, list) {
-    
-        recipes.map(recipe => {
+            if (filterForEachTag[0]) {
+                console.log(recipe)
+                return recipe;
+            }
             
-            // research into recipes names
-            if (recipe.name.toLowerCase().includes(value)) {
-                list.push(recipe)
+            // input content into recipe name or description ?
+            if (recipe.name.toLowerCase().includes(inputContent) ||
+                recipe.description.toLowerCase().includes(inputContent)) {
+                return recipe;
             };
-    
-            // research into recipes appliances
-            if (recipe.appliance.toLowerCase().includes(value)) {
-                list.push(recipe)
+
+            // input content into recipe ingredients ?
+            let filteredRecipesIngredients = recipe.ingredients.filter(ingredient => {
+                if (ingredient.ingredient.toLowerCase().includes(inputContent)) {
+                    return recipe;
+                }
+            });
+
+            if (filteredRecipesIngredients.length > 0) {
+                return true
+            } else {
+                return false;
             };
-    
-            // research into recipes ingredients
-            recipe.ingredients.map(ingredient => {
-                if (ingredient.ingredient.toLowerCase().includes(value)) {
-                    list.push(recipe)
-                } 
-            });
-    
-            // research into recipes ustensils
-            recipe.ustensils.map(ustensil => {
-                if (ustensil.toLowerCase().includes(value)) {
-                    list.push(recipe)
-                } 
-            });
-        })
+
+        });
+
+        
     }
 
-    
-
+    displayRecipes(filteredRecipes);
+    displayDropdownLists(filteredRecipes);
 }
+
+// remove duplicates //https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/
+// let newinputResearchResult = [...new Set(inputResearchResult)]
